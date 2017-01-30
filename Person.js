db81b1218fb0ebf7998e64a10ea7775addc7a2d7
Yura <Nosorog-off@freemail.ru>
2017-01-30 12:16:25 +0200

    koment1234

---
+++ Person.js
@@ -0,0 +1,18 @@
+﻿Ext.define('Classes.Person', {
+    config: {
+            name: 'Eugene',
+            surname : 'Popov'
+    },
+    constructor: function(name, surname) {
+        this.initConfig();
+        if(name){
+            this.setName(name);
+        }
+        if(surname){
+            this.setSurname(surname);
+        }
+    },
+    getinfo: function(){
+        alert("Full NAme : " + this.name + " " + this.surname);
+    },
+  });
---
+++ app.js
@@ -0,0 +1,11 @@
+Ext.onReady(function(){
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    title: ' attachment on Ext JS 4',
+                    html : '<h3>Wellcom to hell Ext JS 4!</h3>'
+                },
+            ]
+        });
+});
